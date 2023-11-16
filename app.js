d85381ae868f10d72f693494edf5d0ffd33668f0
kpatel114 <kpatel114@hawk.iit.edu>
2023-11-16 15:03:03 -0600

    setup the app

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const fileUpload = require("express-fileupload")
+const path = require("path");
+const PORT =process.env.PORT || 3500;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+app.post('/upload',
+    fileUpload({ createParentPath: true }),
+    (req, res) => {
+        const files = req.files
+        console.log(files)
+
+        return res.json({ status: 'logged', message: 'logged' })
+    }
+)
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+
